fa291253ed6489fff9c2464ba10595f0e2c0ad0b
NeerajKumar22 <ineerajkr@gmail.com>
2019-08-27 23:10:42 +0530

    init

---
+++ src/App.js
@@ -0,0 +1,37 @@
+import React from 'react';
+
+import {CardList} from "./components/card-list/card-list.component"
+import './App.css';
+
+class App extends React.Component{
+  constructor(){
+    super();
+    this.state = {
+      monsters : []
+
+    }
+  }
+
+  // Life cycle method
+
+  componentDidMount() {
+    fetch("https://jsonplaceholder.typicode.com/users") // Fetching from this url
+    .then(response => response.json())                  // taking a response and converting into the json format
+    .then(users => this.setState({monsters : users}))   // take the user and set monsters to that array of users
+
+  }
+  
+  render(){
+    return (
+      <div className="App">
+        <CardList name="Neeraj">  
+          {
+            this.state.monsters.map(monster => <h1 key={monster.id}>{monster.name}</h1>)
+          }
+        </CardList>
+      </div>
+    );
+  }
+}
+
+export default App;
